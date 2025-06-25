b76750032971adefb020f1c1c8f3ef935d3d8484
Adri Gomez <127105999+adrigomezz@users.noreply.github.com>
2025-06-25 20:54:20 +0200

    Update Technologies.js

--- components/Technologies.js
+++ components/Technologies.js
@@ -7,7 +7,7 @@ export default function Technologies() {
   const [hoveredCategory, setHoveredCategory] = useState(null);
 
   const technologies = {
-    Frontend: ["React", "Javascript", "HTML", "CSS"],
+    Frontend: ["React", "JavaScript", "HTML", "CSS"],
     Backend: ["Node.js", "Express.js", "MongoDB", "SQL"],
     Frameworks: ["Git", "Firebase", "Linux"],
   };
